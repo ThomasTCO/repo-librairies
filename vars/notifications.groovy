4cfc05070c2af10f86e3de3be1ce676e5a34d579
Thomas Combarnous <thomas.combarnous@gekko.fr>
2020-06-30 17:57:02 +0200

    some libraries

--- vars/notifications.groovy
+++ vars/notifications.groovy
@@ -1,14 +1,15 @@
 #!/usr/bin/env groovy
 import groovy.json.JsonOutput
 
-def call(text, channel, urlSlack) {
+void notify(text, channel, urlSlack, attachements) {
     def slackURL = urlSlack
     def jenkinsIcon = 'https://wiki.jenkins-ci.org/download/attachments/2916393/logo.png'
 
     def payload = JsonOutput.toJson([text: text,
         channel: channel,
         username: "Jenkins",
-        icon_url: jenkinsIcon
+        icon_url: jenkinsIcon,
+        attachement: attachements
     ])
 
     sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
