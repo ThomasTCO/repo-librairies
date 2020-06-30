474d80b32e38f47974bae8c8b11854f30488fdee
Thomas Combarnous <thomas.combarnous@gekko.fr>
2020-06-30 16:51:06 +0200

    some libraries

--- vars/notifications.groovy
+++ vars/notifications.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 import groovy.json.JsonOutput
 
-def notifySlack(text, channel, urlSlack, attachments) {
+def call(text, channel, urlSlack, attachments) {
     def slackURL = slackurlSlackUrl
     def jenkinsIcon = 'https://wiki.jenkins-ci.org/download/attachments/2916393/logo.png'
 
